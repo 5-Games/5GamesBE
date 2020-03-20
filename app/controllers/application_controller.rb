c9948bd3cfe00e43c6fecccbcf502a0514b5b22c
Stephen Keating <stephen.keating@gmail.com>
2020-03-20 11:55:13 -0400

    auth done in rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  
+  # Need to hide the secret in an ENV file for production:
+  # https://www.viget.com/articles/storing-secret-credentials-in-rails-5-2-and-up/
+  def secret
+    ENV['JWTsecret']
+  end
+
 end
