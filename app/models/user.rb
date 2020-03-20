0a13fa747b6ef19e74a17aab5a596c5c6e70bd4f
Stephen Keating <stephen.keating@gmail.com>
2020-03-20 09:21:57 -0400

    setting up auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # has_secure_password authenticates passwords with bcrypt
+  has_secure_password
+
+  # Make sure all user names are unique
+  validates :username, uniqueness: true
+
 end
